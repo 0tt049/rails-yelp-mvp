d0c24fd2ac49866aeccdbc21d66076b3e23049a3
Otavio Coelho <otavio.gmcoelho@gmail.com>
2023-02-24 14:35:11 -0300

    Implemented all the way to Seed

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,37 @@
+class RestaurantsController < ApplicationController
+  # before_action :set_restaurant, only: %i[show edit update destroy]
+
+  # def index
+  #   @restaurants = Restaurant.all
+  # end
+
+  # def show; end
+
+  # def new
+  #   @restaurant = Restaurant.new
+  # end
+
+  # def create
+  #   @restaurant = Restaurant.new(restaurants_params)
+  # end
+
+  # def edit; end
+
+  # def update
+  #   ##
+  # end
+
+  # def destroy
+  #   @restaurant.destroy
+  # end
+
+  # private
+
+  # def restaurant_params
+  #   ##
+  # end
+
+  # def set_restaurant
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
+end
